c6ff0d93bac5206a1e7111408d574348264b17d6
Seesaw <weiseesaw@gmail.com>
2024-04-23 20:36:07 -0400

    feat: index mobile view

--- js/on_load.js
+++ js/on_load.js
@@ -1,4 +1,18 @@
-window.onload = function() {
+// window.onload = function() {
+//     var element = document.getElementById("welcome");
+//     element.style.margin = "100px";  
+// };
+
+window.onload = adjustMargin;
+window.onresize = adjustMargin; 
+
+function adjustMargin() {
     var element = document.getElementById("welcome");
-    element.style.margin = "100px";  
-};
+    if (window.innerWidth > 900) { 
+        element.style.margin = "100px";
+    } else if (window.innerWidth > 480) { // Tablets
+        element.style.margin = "50px";
+    } else { // Mobile phones
+        element.style.margin = "20px";
+    }
+}
